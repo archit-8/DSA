743dd0d5809bba1c6b498911204906abe85af850
Archit Singh Chauhan <88979004+archit-8@users.noreply.github.com>
2026-07-30 16:57:47 +0530

    Add brute force method to calculate max area
    Implemented a brute force approach to calculate the maximum area formed by two lines in the given height array.

--- Containerwithwaiter.java
+++ Containerwithwaiter.java
@@ -1,1 +1,21 @@
+#Brute Force approch 
+class Solution {
+    public int maxArea(int[] height) {
 
+        int maxArea = 0;
+
+        for (int i = 0; i < height.length - 1; i++) {
+
+            for (int j = i + 1; j < height.length; j++) {
+
+                int width = j - i;
+                int minHeight = Math.min(height[i], height[j]);
+                int area = width * minHeight;
+
+                maxArea = Math.max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+}
